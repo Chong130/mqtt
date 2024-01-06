fd91ce1db12c0f1a5081eb7d3cee7c0650f12c05
Chong130 <112062203+Chong130@users.noreply.github.com>
2024-01-06 19:54:03 +0800

    Update demo.js

--- demo.js
+++ demo.js
@@ -10,8 +10,8 @@ function startConnect(){
     document.getElementById("messages").innerHTML += "<span> Connecting to " + host + "on port " +port+"</span><br>";
     document.getElementById("messages").innerHTML += "<span> Using the client Id " + clientID +" </span><br>";
 
-    client = new Paho.MQTT.Client(host,Number(port),clientID);
-    // client = new Paho.MQTT.Client("wss://mqtt.eclipse.org", clientID);
+    // client = new Paho.MQTT.Client(host,Number(port),clientID);
+    client = new Paho.MQTT.Client("wss:/mqtt-dashboard.com", 8884 ,clientID);
 
     client.onConnectionLost = onConnectionLost;
     client.onMessageArrived = onMessageArrived;
